af2cf6298726da0f11195cd8ce5cf0a5c91c76f3
Bora Lee <bbora1119@gmail.com>
2021-09-12 23:14:14 -0400

    complete

---
+++ Develop/script.js
@@ -0,0 +1,69 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+ 
+}
+
+function generatePassword() {
+  var length = prompt('Password length : Enter 8 to 128');
+
+  if(length<8){
+    alert('Password should be greater than 8.');
+    return '';
+  } else if(length>128){
+    alert('Password should be less than 128.')
+    return '';
+  } else {
+
+  var option = [];
+
+
+  if (confirm('Do you want lowercase to include in your password?') == true){
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    option.push(lowercase);
+  }
+
+
+  if (confirm('Do you want uppercase to include in your password?') == true){
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    option.push(uppercase);  
+  }
+   
+  if (confirm('Do you want numeric to include in your password?') == true){
+    var numeric = '1234567890';
+    option.push(numeric);
+  }
+  
+  if (confirm('Do you want special characters to include in your password?') == true){
+    var specialChar = "!@#$%^&*()-=+[]'?/<>";
+    option.push(specialChar);  
+  }
+
+  if (option.length === 0) {
+    alert('Choose at least one character type.')
+    return '';
+  }
+
+
+  var RandomPassword = "";
+
+  for(let i=0; i<length; i++){
+    var selector = option.join('');
+    RandomPassword += selector[Math.floor(Math.random() * selector.length)]; 
+  }
+
+ 
+  return RandomPassword;
+
+  }
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Homework3_Javascript
+
+<Password Generator>
+
+1. You can generate a confidential password in this web page.
+
+2. Choose length of password you want between 8 to 128.
+
+3. You have to choose at least charactor types among lowercases, uppercase, numeric and special charactoers.
+
+4. After select each types you want, you can see the password generated.
+
+![Password Generator](./Develop/Password_Generator.gif)
